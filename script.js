ceda54f7d5058fb86498edcdb04d06c92313d969
Naet27 <https://github.com/Naet27/Cake.git>
2025-09-30 20:53:01 +0700

    update code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ============ COUNTDOWN ============
-const birthday = new Date("2025-09-01T00:00:00");
+const birthday = new Date("2025-10-01T00:00:00");
 
 function updateCountdown(){
   const now = new Date();
